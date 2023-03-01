69415747b6b09ad580b9e122f895fe6030e05ed7
DanOlinski <daniel.guterres@hotmail.com>
2023-03-01 16:37:35 -0700

    installed nodemon

--- express_server.js
+++ express_server.js
@@ -7,13 +7,20 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
+});//access: http://localhost:8080/
 
+//this shows to the client the urlDatabase object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
+});//access: http://localhost:8080/urls.json
+
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});//access: http://localhost:8080/hello
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
