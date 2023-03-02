261dc48e22cd9c03abfc4a03b9c0b61571a3dce6
DanOlinski <daniel.guterres@hotmail.com>
2023-03-01 17:45:35 -0700

    implemented ejs file

--- express_server.js
+++ express_server.js
@@ -1,27 +1,37 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080; // default port
 
+//declaration below tells express.set module that HTML code will be rendered by ejs
+app.set("view engine", "ejs")
+
+//keep track of all URLs and shortened forms
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//Set the file for ejs to render from
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
-});//access: http://localhost:8080/
+});
 
 //this shows to the client the urlDatabase object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});//access: http://localhost:8080/urls.json
+})
 
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-});//access: http://localhost:8080/hello
+});
 
+//the method below sets the server to listen at predefined port 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
